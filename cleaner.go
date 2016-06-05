fca14842c157c8dbc6b14e26cab83f68eee269cb
ElyKar <tristan.claverie@laposte.net>
2016-06-05 11:50:21 +0200

    Optimize path cleaning

--- cleaner.go
+++ cleaner.go
@@ -1,29 +1,112 @@
-package main
+package whitetail
 
-import (
-	"fmt"
-	"strings"
-)
-
+// Cleans the path given.
+// Rules for cleaning:
+//
+// - All duplicate slashes are removed ( '//' -> '/')
+// - Possibility to navigate the path with '..' and '.' (cannot go beyond '/')
+// - If there is no leading slash, add it
+//
+// If no cleaning is necessary (as it is likely), just returns the given path.
 func clean(path string) string {
-	elements := strings.Split(path, "/")
-	fmt.Printf("Path is %s\n", path)
-	fmt.Printf("Split: %v\n", elements)
-	var l int
-	res := make([]string, 0)
-	for _, elt := range elements {
-		if elt == ".." {
-			if len(res) < 1 {
-				l = 0
-			} else {
-				l = len(res) - 1
+
+	if path == "" {
+		return "/"
+	}
+
+	if is_clean(path) {
+		return path
+	}
+
+	n := len(path)
+	var buf []byte
+
+	r := 1
+	w := 1
+
+	if path[0] != '/' {
+		buf = make([]byte, n+1)
+		r = 0
+	} else {
+		buf = make([]byte, n)
+	}
+	buf[0] = '/'
+
+	trailing := n > 2 && path[n-1] == '/'
+
+	for r < n {
+
+		switch {
+
+		// Remove duplicate slashes: do nothing
+		case path[r] == '/':
+			r++
+
+		// The path ends with '/.'
+		case path[r] == '.' && r == n-1:
+			trailing = true
+			r++
+
+		// The path ends with '/.'
+		case path[r] == '.' && path[r+1] == '/':
+			r++
+
+		// Handle the .. element
+		case path[r] == '.' && path[r+1] == '.' && (r+1 == n-1 || path[r+2] == '/'):
+			r += 2
+			if w != 1 {
+				w--
+				for w > 0 && buf[w-1] != '/' {
+					w--
+				}
 			}
-			res = res[:l]
-		} else if elt == "." || strings.TrimLeft(elt, ".") == "" {
-			continue
+
+			// Path ends with ".."
+			if r == n {
+				trailing = true
+			}
+
+		default:
+
+			for r < n && path[r] != '/' {
+				buf[w] = path[r]
+				w++
+				r++
+			}
+
+			if r < n || trailing {
+				buf[w] = '/'
+				w++
+			}
+
+		}
+
+	}
+
+	return string(buf[:w])
+}
+
+func is_clean(path string) bool {
+
+	if path[0] != '/' {
+		return false
+	}
+
+	n := len(path)
+	var cur int
+
+	for cur < n {
+
+		if path[cur] != '.' {
+			cur++
 		} else {
-			res = append(res, elt)
+			if path[cur-1] == '/' && (cur == n-1 || path[cur+1] == '/' || (path[cur+1] == '.' && (cur+2 == n-1 || path[cur+2] == '/'))) {
+				return false
+			} else {
+				cur += 2
+			}
 		}
 	}
-	return "/" + strings.Join(res, "/")
+
+	return true
 }
